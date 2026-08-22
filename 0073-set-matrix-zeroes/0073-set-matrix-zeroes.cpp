dd36d366131e6e66a5dd58c1c980be4b5eb58f04
Vipul Mishra <vipul2006mishra@gmail.com>
2026-08-22 20:56:41 +0530

    Time: 0 ms (100%), Space: 21.1 MB (25.29%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        bool firstRow = false, firstCol = false;
+
+        // Check if first row has zero
+        for (int j = 0; j < n; j++) 
+            if (matrix[0][j] == 0) firstRow = true;
+
+        // Check if first column has zero
+        for (int i = 0; i < m; i++) 
+            if (matrix[i][0] == 0) firstCol = true;
+
+        // Use first row & col as markers
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Zero out cells based on markers
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Handle first row
+        if (firstRow) {
+            for (int j = 0; j < n; j++) matrix[0][j] = 0;
+        }
+
+        // Handle first column
+        if (firstCol) {
+            for (int i = 0; i < m; i++) matrix[i][0] = 0;
+        }
+    }
+};
