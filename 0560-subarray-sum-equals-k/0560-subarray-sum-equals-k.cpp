278ddeb1a616200483973c59d27c965cc0e0a182
Vipul Mishra <vipul2006mishra@gmail.com>
2026-08-24 21:30:54 +0530

    Time: 28 ms (98.26%), Space: 45.5 MB (39.26%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,32 +3,22 @@ using namespace std;
 
 class Solution {
 public:
-    // Function to find count of subarrays with sum equal to k
-    int subarraySum(vector<int>& arr, int k) {
-        // Size of the array
-        int n = arr.size();
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> prefixCount; 
+        prefixCount[0] = 1; // base case
 
-        // Initialize count of subarrays
-        int count = 0;
+        int sum = 0, count = 0;
+        for (int x : nums) {
+            sum += x;
 
-        // Traverse all possible start indices
-        for (int i = 0; i < n; i++) {
-            // Initialize sum for current subarray
-            int sum = 0;
-
-            // Traverse all possible end indices from start
-            for (int j = i; j < n; j++) {
-                // Add current element to sum
-                sum += arr[j];
-
-                // If sum equals k, increment count
-                if (sum == k) {
-                    count++;
-                }
+            // Check if (sum - k) exists in map
+            if (prefixCount.find(sum - k) != prefixCount.end()) {
+                count += prefixCount[sum - k];
             }
-        }
 
-        // Return total count of subarrays
+            // Store current prefix sum
+            prefixCount[sum]++;
+        }
         return count;
     }
 };
